87f155e984fea1229127f53da6c4f7ac8946b675
springframe <logtomansur786@gmail.com>
2023-07-09 10:13:08 +0530

    first commit

---
+++ src/test/java/com/temp/app/TempAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temp.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
